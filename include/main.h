9a8d786c50c85c2586b7937e9cb4724ebf213c40
Hiram Benroth <hiram.benroth@gmail.com>
2025-05-15 03:46:33 -0600

    First Commit
    A small interface in Platformio for the esp32 board.

---
+++ include/main.h
@@ -0,0 +1,25 @@
+#include "driver/gpio.h"    //GPIO Driver
+#include "driver/i2c.h"     //I2C Driver
+
+/* *** PIN ASSIGNMENT DEFINES *** */
+
+// Pins 22 and 21 support i2c 
+#define I2C_MASTER_SCL_IO GPIO_NUM_22 
+#define I2C_MASTER_SDA_IO GPIO_NUM_21
+
+// Pins for Motors 
+
+// Bluetooth
+
+/* *** END PIN ASSIGNMENT DEFINES *** */
+
+/* *** PRIVATE DEFINES START *** */
+#define THE_SKY_IS_BLUE 1
+/* *** PRIVATE DEFINES START *** */
+
+/* *** INITIALIZE FUNCTIONS DECLARATIONS START *** */
+void Init_I2C(void ); //I2C Initializer
+
+
+
+/* *** END INITIALIZE FUNCTIONS DECLARATIONS *** */
---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include "main.h"
+//pin assignments look at main.h
+
+
+
+void app_main() {
+    //Initialize all things
+    Init_I2C();
+
+
+
+    // Infinate while loop
+    while(THE_SKY_IS_BLUE){
+
+    } ;
+}
+
+
+
+
+/* *** INITIALIZE FUNCTIONS DEFINITIONS START *** */
+
+   
+//This function initializes i2c protocal with the defined pin in main.h
+void Init_I2C(void) {
+     i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 100000
+    };
+    i2c_param_config(I2C_NUM_0, &conf);
+    i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
+}
+
+/* *** INITIALIZE FUNCTIONS DEFINITIONS END *** */
