cc3702178d8affd752b5d7f4ef9381af8f2df8b6
Hiram Benroth <hiram.benroth@gmail.com>
2025-05-22 12:26:56 -0600

    Pin assignments

--- include/main.h
+++ include/main.h
@@ -13,13 +13,32 @@
 
 /* *** PIN ASSIGNMENT DEFINES *** */
 
-// Pins 22 and 21 support i2c 
-#define I2C_MASTER_SCL_IO GPIO_NUM_22 
-#define I2C_MASTER_SDA_IO GPIO_NUM_21
+// pin_config.h - Generated Pin Configuration for ESP32-S2
+// Platform: espressif-dev-32 (ESP-IDF)
 
-// Pins for Motors 
-
-// Bluetooth
+// PIN_EN (Pin 1): EN - Reset
+// PIN_VIN (Pin 3): vin - Power
+#define L_ULTRA_TRIG GPIO_NUM_32 // Pin 6: Digital Output
+#define L_ULTRA_ECHO GPIO_NUM_33 // Pin 7: Digital Input
+#define R_ULTRA_TRIG GPIO_NUM_25 // Pin 8: Digital Output
+#define R_ULTRA_ECHO GPIO_NUM_26 // Pin 9: Digital Input
+#define L_MOTOR_DIR GPIO_NUM_27 // Pin 10: Digital Input
+#define L_MOTOR_PWM GPIO_NUM_14 // Pin 11: Digital Input
+#define R_MOTOR_DIR GPIO_NUM_12 // Pin 12: Digital Input
+#define R_MOTOR_PWM GPIO_NUM_13 // Pin 13: Digital Input
+// PIN_GND (Pin 14): GND - Ground
+// PIN_VIN (Pin 15): VIN - Power
+#define I2C_MASTER_SCL_IO GPIO_NUM_22 // Pin 17: I2C_SCL
+#define PIN_RX0 GPIO_NUM_44 // Pin 19: Digital I/O
+#define I2C_MASTER_SDA_IO GPIO_NUM_21 // Pin 20: I2C_SDA
+#define H_BRIDGE_L_ENABLE GPIO_NUM_5 // Pin 23: Output
+#define H_BRIDGE_L_IN1 GPIO_NUM_17 // Pin 24: Digital Output
+#define H_BRIDGE_L_IN2 GPIO_NUM_16 // Pin 25: Digital Output
+#define H_BRIDGE_R_ENABLE GPIO_NUM_4 // Pin 26: Digital Output
+#define H_BRIDGE_R_IN1 GPIO_NUM_2 // Pin 27: Digital Output
+#define H_BRIDGE_R_IN2 GPIO_NUM_15 // Pin 28: Digital Output
+// PIN_GND (Pin 29): GND - Ground
+// PIN_3V3 (Pin 30): 3V3 - Power
 
 /* *** END PIN ASSIGNMENT DEFINES *** */
 
